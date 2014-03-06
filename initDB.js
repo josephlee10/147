2258e23b896ca5a3d5cde950d7e7731fd5eeddcf
Lea Hyun Kim <leahkim@stanford.edu>
2014-03-06 14:05:50 -0800

    connecting to MongoDB working on the profile view page

--- initDB.js
+++ initDB.js
@@ -25,7 +25,9 @@ mongoose.connect(database_uri);
 // Do the initialization here
 
 // Step 1: load the JSON data
-var projects_json = require('./data.json');
+var allData_json = require('./data.json');
+var projects_json = allData_json.Projects;
+var users_json = allData_json.Users;
 
 // Step 2: Remove all existing documents
 models.Project
@@ -50,7 +52,25 @@ function onceClear(err) {
       to_save_count--;
       console.log(to_save_count + ' left to save');
       if(to_save_count <= 0) {
-        console.log('DONE');
+        console.log('Food DONE');
+        // The script won't terminate until the 
+        // connection to the database is closed
+      }
+    });
+  }
+
+  to_save_count = users_json.length;
+  for(var i=0; i<users_json.length; i++) {
+    var json = users_json[i];
+    var proj = new models.User(json);
+
+    proj.save(function(err, proj) {
+      if(err) console.log(err);
+
+      to_save_count--;
+      console.log(to_save_count + ' left to save');
+      if(to_save_count <= 0) {
+        console.log('User DONE');
         // The script won't terminate until the 
         // connection to the database is closed
         mongoose.connection.close()
--- routes/favs.js
+++ routes/favs.js
@@ -1,36 +1,54 @@
 var models = require('../models');
 
-exports.view = function(req, res){ // This will show the pictures 'guest' has uploaded
-
-  models.Project
+exports.view = function(req, res){ // This will render what the user uploaded and liked
+  models.Project // get the user uploads
   .find({"username": "guest"})
   .exec(renderProjects);
 
   function renderProjects(err, projects) {
-    res.render('myFavs', {'allFoods':projects});   
+    var nUploads = projects.length;
+
+    models.User
+          .find({"usr_name": "guest"}, function (err, docs) {
+              console.log(docs[0]);
+          });
+
+    if (nUploads > 0)
+      res.render('myFavs', {'showUploads': true,
+                            'allFoods':projects});   
+    else
+      res.render('myFavs', {'showUploads': false});
   }
 };
 
-exports.viewLikedFoods = function(req, res) { // This will show the pictures 'guest' has liked
-
-};
-
-exports.likedFoodInfo = function(req, res) {
+exports.likedFoodInfo = function(req, res) { // This will add 1 to the total likes and push the foodID to the user liked section
   var foodID = req.params.id;
 
   models.Project.find({"_id":foodID}, function(err, docs){ 
-      var totalLikes = docs[0].likes;
+      var totalLikes = docs[0].likes + 1;
       models.Project
-            .update({"_id": foodID}, {"likes": totalLikes})
-            .exec(afterUpdating);
+            .update({"_id": foodID}, {"likes": totalLikes});
+      models.User
+            .find({"usr_name": "guest"}, function(err, docs){
+                console.log(docs[0]);
+                var allLiked = docs[0].liked;
+                allLiked.push({
+                  "food_id": foodID
+                });
+
+                models.User
+                      .update({"usr_name": "guest"}, {"liked": allLiked})
+                      .exec(afterUpdating);
+            });
   });
   
   function afterUpdating(err) {
+    if (err) {console.log(err); res.send(500)};
     res.send(foodID);
   }
 };
 
-exports.undoLike = function(req, res) {
+exports.undoLike = function(req, res) { // need to work on this
   var foodID = req.params.id;
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
@@ -46,7 +64,15 @@ exports.undoLike = function(req, res) {
   res.send(foodID);
 };
 
-exports.eraseMyUpload = function(req, res) {
-  var foodID = req.params.id;
-  res.send(foodID);
+exports.eraseMyUpload = function(req, res) { // This will erase the upload
+  var foodID = req.query.id;
+  models.Project
+        .find({"_id": foodID})
+        .remove()
+        .exec(afterRemoving);
+
+  function afterRemoving(err) {
+      if (err) {console.log(err); res.send(500);}
+      res.send(foodID);
+  }
 };
